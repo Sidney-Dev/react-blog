d8e55e7eb8c5d62a3af07348c617fffb9b0e99ca
Sidney de Sousa <esp.sousa@gmail.com>
2022-01-15 15:10:30 +0200

    remoed delete option

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,21 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
    
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
-
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
         .then(res => res.json())
-        .then(data => setBlogs(data))
+        .then(data => {
+            setBlogs(data)
+            setIsPending(false)
+        })
     }, [])
 
     return ( 
         <div className="home">
-            {blogs && <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>}
+            { isPending && <div className="loading">Loading...</div> }
+            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
     );
 }
