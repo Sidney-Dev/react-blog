2be8cf76436680b6d94051c7d8ed9f49744921df
Sidney de Sousa <esp.sousa@gmail.com>
2022-01-16 09:18:12 +0200

    custom hook and fetch api refactoring

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,11 @@
 import { useEffect, useState } from 'react'
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-   
-    useEffect(() => {
-        fetch("http://localhost:8000/blogss")
-        .then(res => {
-            if(!res.ok) {
-                throw Error("Could not fetch the data for the resource")
-            }
-            return res.json()
-        })
-        .then(data => {
-            setBlogs(data)
-            setIsPending(false)
-            setError(null)
-        })
-        .catch(e => {
-            setError(e.message)
-            setIsPending(false)
-        })
-        
-    }, [])
-
+    const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs")
+ 
     return ( 
         <div className="home">
             { error && <div>{ error }</div> }
