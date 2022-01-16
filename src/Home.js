11734e4f0f6950e0ebdfbc059026f98410ff5553
Sidney de Sousa <esp.sousa@gmail.com>
2022-01-16 09:08:33 +0200

    fetch api in useEffect

--- src/Home.js
+++ src/Home.js
@@ -5,18 +5,31 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
     const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
    
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-        .then(res => res.json())
+        fetch("http://localhost:8000/blogss")
+        .then(res => {
+            if(!res.ok) {
+                throw Error("Could not fetch the data for the resource")
+            }
+            return res.json()
+        })
         .then(data => {
             setBlogs(data)
             setIsPending(false)
+            setError(null)
         })
+        .catch(e => {
+            setError(e.message)
+            setIsPending(false)
+        })
+        
     }, [])
 
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
             { isPending && <div className="loading">Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
